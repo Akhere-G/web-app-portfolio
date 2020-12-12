5b5e8a6b88eb14a2289a8f15c87a478e669485dd
Akhere Ihoeghinlan <zcabaih@ucl.ac.uk>
2020-12-12 23:08:15 +0000

    changed description

--- src/data.js
+++ src/data.js
@@ -10,7 +10,7 @@ const data = [
     desc: `A chat app built using react for the front end and firebase for the 
     authentication and the database. The app allows users to login and 
     send messages to a public group chat. The user can delete their own 
-    messages as well.`,
+    messages as well. It is responsive and  mobile friendly.`,
     github: "https://github.com/Akhere-G/chat-app",
     technologies: ["React", "Firebase"],
   },
@@ -18,9 +18,10 @@ const data = [
     title: "Movie Search",
     url: "https://ai-search-movie.netlify.app/about",
     img: movieSearchImg,
-    desc: ` This website is built using react for the front end. The api used is 
-  themoviedb. A global context is used to manage state and a high order 
-  component is used to load images with a fade in animation.`,
+    desc: `This website is built using react for the front end. The API used is 
+    Themoviedb. A global context is used to manage state and a higher order component 
+    is used to load images with a fade in animation. A pagination components is used to display 
+    the movies in the given space.`,
 
     github: "https://github.com/Akhere-G/movie-search-app",
     technologies: ["React"],
@@ -29,8 +30,9 @@ const data = [
     title: "Blog Site",
     url: "https://ai-blog-site.netlify.app/",
     img: blogSiteImg,
-    desc: `A blog site that displays markdown blogs. This website is built using 
-    Gatsby for the front end and Contentful for the CMS.`,
+    desc: `A blog site that displays markdown blog posts. This website is built using 
+    Gatsby for the front end and Contentful for the CMS. It is responsive and allows blog 
+    posts with images to be shown`,
 
     github: "https://github.com/Akhere-G/gatsby-blog-site",
     technologies: ["React", "Gatsby", "Contentful"],
