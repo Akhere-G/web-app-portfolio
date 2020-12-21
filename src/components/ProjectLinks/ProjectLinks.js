f02a2fcc48165bb032c03e201164ca463f9528a0
Akhere Ihoeghinlan <zcabaih@ucl.ac.uk>
2020-12-21 00:26:49 +0000

    added responsive project header

--- src/components/ProjectLinks/ProjectLinks.js
+++ src/components/ProjectLinks/ProjectLinks.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import styles from "./ProjectLinks.module.css";
 import { Link } from "react-scroll";
+import MenuIcon from "@material-ui/icons/Menu";
+
 const ProjectLinks = ({ projectLinks }) => {
   const [isMoving, setIsMoving] = useState(false);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     const scrollListener = () => {
       const scrollY = window.scrollY;
-      if (scrollY > 0) {
+      if (scrollY > 50) {
         setIsMoving(true);
       } else {
         setIsMoving(false);
@@ -22,17 +25,32 @@ const ProjectLinks = ({ projectLinks }) => {
   }, []);
 
   return (
-    <ul className={`${styles.projectLinks} ${isMoving ? styles.moving : ""}`}>
-      {projectLinks.map((link, index) => {
-        return (
-          <li key={index}>
-            <Link to={`${link.slug}`} smooth={true} duration={1000}>
-              {link.title}
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
+    <div className={`${styles.projectLinks} ${isMoving ? styles.moving : ""}`}>
+      <ul
+        className={`${styles.modal} ${
+          open ? styles.openModal : styles.closeModal
+        }`}
+      >
+        {projectLinks.map((link, index) => {
+          return (
+            <li key={index}>
+              <Link to={`${link.slug}`} smooth={true} duration={1000}>
+                {link.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+
+      <div
+        className={styles.toggleBtn}
+        onClick={() => {
+          setOpen(prev => !prev);
+        }}
+      >
+        <MenuIcon />
+      </div>
+    </div>
   );
 };
 
