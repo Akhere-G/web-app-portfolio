809a73a236a2643c593ee040cf2377320325f0a1
Akhere Ihoeghinlan <zcabaih@ucl.ac.uk>
2021-01-17 20:17:04 +0000

    changed navbar styles. Added proptypes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,51 +1,84 @@
 import React, { useState } from "react";
 import styles from "./Navbar.module.css";
 import { Link, useLocation } from "react-router-dom";
-import MenuIcon from "@material-ui/icons/Menu";
+import { Menu } from "@material-ui/icons";
+import PropTypes from "prop-types";
 
-const Navbar = () => {
+const Navbar = ({ links, title }) => {
   const [open, setOpen] = useState(false);
   const { pathname } = useLocation();
 
-  const links = [
-    { title: "home", slug: "" },
-    { title: "about", slug: "/about" },
-    { title: "contact", slug: "/contact" },
-  ];
   return (
-    <div className={styles.navbar}>
-      <Link to='/'>
-        <h1>Akhere Ihoeghinlan</h1>
-      </Link>
-      <ul
-        className={`${styles.modal} ${
-          open ? styles.openModal : styles.closeModal
-        }`}
-      >
-        {links.map((link, index) => {
-          return (
-            <li key={index}>
-              <Link
-                to={`${link.slug}`}
-                className={`${pathname === link.slug ? "active" : ""}`}
-              >
-                {link.title}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+    <header
+      className={styles.container}
+      onMouseLeave={() => {
+        setOpen(false);
+      }}
+      onBlurCapture={e => {
+        if (!e.currentTarget.contains(e.relatedTarget)) {
+          setOpen(false);
+          console.log("hit");
+        }
+      }}
+    >
+      <nav className={styles.navbar}>
+        <h1>
+          <Link
+            to='/'
+            className={styles.logo}
+            onClick={() => {
+              setOpen(false);
+            }}
+          >
+            {title}
+          </Link>
+        </h1>
 
-      <div
-        className={styles.toggleBtn}
-        onClick={() => {
-          setOpen(prev => !prev);
-        }}
-      >
-        <MenuIcon />
-      </div>
-    </div>
+        <ul className={`${styles.navbarLinks} ${open ? styles.open : ""}`}>
+          {links.map((link, index) => {
+            const { name, slug } = link;
+            const activeClass = pathname === slug ? styles.activeLink : "";
+            return (
+              <li key={index} className={`${styles.navbarLink} ${activeClass}`}>
+                <Link
+                  to={slug}
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                >
+                  {name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+        {links.length > 0 && (
+          <button
+            className={styles.navbarBtn}
+            onClick={() => {
+              setOpen(prev => !prev);
+            }}
+          >
+            <Menu />
+          </button>
+        )}
+      </nav>
+    </header>
   );
 };
 
+Navbar.propTypes = {
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      slug: PropTypes.string.isRequired,
+    }).isRequired
+  ),
+  title: PropTypes.string.isRequired,
+};
+
+Navbar.defaultProps = {
+  links: [],
+  title: "",
+};
 export default Navbar;
